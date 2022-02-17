39baee7051b15d4d3558e53cfad268d9df463726
Seema27 <52322791+Seema27@users.noreply.github.com>
2022-02-17 13:09:42 +0530

    Update firstPage.js

--- firstPage.js
+++ firstPage.js
@@ -1,3 +1,36 @@
-$( document ).ready(function() {
-    console.log( "ready!" );
-});
+
+        //$('#click').click(function(){
+    $.ajax({
+            "async": true,
+            "crossDomain": false,
+            "url": "https://partial-welink1.cs197.force.com/services/apexrest/scheduleServiceAppointment"
+
+$(document).ready(function() {
+    //$('#click').click(function(){
+    $.ajax({
+            "async": true,
+            "crossDomain": false,
+            "url": "https://partial-welink1.cs197.force.com/services/apexrest/scheduleServiceAppointment",
+            "method": "POST",
+            "headers": {
+                "content-type": "application/x-www-form-urlencoded",
+                "cache-control": "no-cache",
+                "postman-token": "ecf50a87-1531-23b7-900d-d79cd4163d1f"
+            },
+            "data": {
+                "opName": "Access Token",
+                "customerData": "{}",
+                
+            },
+            success: function(res) {
+                console.log(res);
+            },
+            error: function() {
+
+
+            });
+        //});
+
+        
+    });
+
