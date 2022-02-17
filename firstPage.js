da4af9b0038d1e2fa432a6343ebdc7fe57697776
smzord <91662040+smzord@users.noreply.github.com>
2022-02-17 13:55:34 +0530

    Update firstPage.js

--- firstPage.js
+++ firstPage.js
@@ -1,28 +1,23 @@
-$(document).ready(function() {
-      //$('#click').on('click',function(){
-      $.ajax({
-          "async": true,
-          "crossDomain": false,
-          "url": "https://partial-welink1.cs197.force.com/services/apexrest/scheduleServiceAppointment",
-          "method": "POST",
-          "headers": {
-            "content-type": "application/x-www-form-urlencoded",
-            "cache-control": "no-cache",
-            "postman-token": "ecf50a87-1531-23b7-900d-d79cd4163d1f"
-          },
-          "data": {
-            "opName": "Access Token",
-            "customerData": "{}",
-
-          },
-          success: function(res) {
-            console.log('==res==', res);
-          },
-          error: function(err) {
-            console.log('==err==', err);
-
-          }
-         });
-        //});
-	});
-
+$(document).ready(function () {
+  $.ajax({
+    async: true,
+    crossDomain: false,
+    url: "https://partial-welink1.cs197.force.com/services/apexrest/scheduleServiceAppointment",
+    method: "POST",
+    headers: {
+      "content-type": "application/x-www-form-urlencoded",
+      "cache-control": "no-cache",
+      "postman-token": "ecf50a87-1531-23b7-900d-d79cd4163d1f",
+    },
+    data: {
+      opName: "Access Token",
+      customerData: "{}",
+    },
+    success: function (res) {
+      console.log("==res==", res);
+    },
+    error: function (err) {
+      console.log("==err==", err);
+    },
+  });
+});
