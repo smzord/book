32a4ca095107fc482ada73f8e2a4a643378b6aa7
smzord <91662040+smzord@users.noreply.github.com>
2022-02-17 13:46:17 +0530

    Update firstPage.js

--- firstPage.js
+++ firstPage.js
@@ -1,36 +1,28 @@
-
-        //$('#click').click(function(){
-    $.ajax({
-            "async": true,
-            "crossDomain": false,
-            "url": "https://partial-welink1.cs197.force.com/services/apexrest/scheduleServiceAppointment"
-
 $(document).ready(function() {
-    //$('#click').click(function(){
-    $.ajax({
-            "async": true,
-            "crossDomain": false,
-            "url": "https://partial-welink1.cs197.force.com/services/apexrest/scheduleServiceAppointment",
-            "method": "POST",
-            "headers": {
-                "content-type": "application/x-www-form-urlencoded",
-                "cache-control": "no-cache",
-                "postman-token": "ecf50a87-1531-23b7-900d-d79cd4163d1f"
-            },
-            "data": {
-                "opName": "Access Token",
-                "customerData": "{}",
-                
-            },
-            success: function(res) {
-                console.log(res);
-            },
-            error: function(err) {
-                         console.log(err);
+      //$('#click').on('click',function(){
+      $.ajax({
+          "async": true,
+          "crossDomain": false,
+          "url": "https://partial-welink1.cs197.force.com/services/apexrest/scheduleServiceAppointment",
+          "method": "POST",
+          "headers": {
+            "content-type": "application/x-www-form-urlencoded",
+            "cache-control": "no-cache",
+            "postman-token": "ecf50a87-1531-23b7-900d-d79cd4163d1f"
+          },
+          "data": {
+            "opName": "Access Token",
+            "customerData": "{}",
 
-            });
+          },
+          success: function(res) {
+            console.log('==res==', res);
+          },
+          error: function(err) {
+            console.log('==err==', err);
+
+          }
+         });
         //});
-
-        
-    });
+	});
 
